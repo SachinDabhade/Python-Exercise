b9e6d086831c9230f89ecf57bc15819518d51f1d
Sachin Vinayak Dabhade <78628027+SachinDabhade@users.noreply.github.com>
2022-09-02 22:31:19 +0530

    deleting

--- Python Exercises/Python_Server.py
+++
@@ -1,9 +0,0 @@
-# This is the python programme to make the small server in python instead of locak html file or any big server
-""" How to make servers into the python programmes to run websites """
-"""
-1. We recommend you to use sublime text for web development 
-2. Open the folder in sublime text and make the html file
-3. same folder or file open in power-shell and run the command - 'python -m http.server --bind=localhost' or 'python -m http.server' amd open chrome and search for localhost:8000
-4. That's done from you...!
-Is it html or http
-"""
